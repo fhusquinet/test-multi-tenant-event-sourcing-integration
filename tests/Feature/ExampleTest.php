62f19a43f1f1a0c5ffb36e2ba5f9906823cb037b
Florian Husquinet <florian.husquinet@deegital.be>
2019-12-27 10:01:33 +0100

    Demo

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest2()
+    {
+        $this->assertTrue(true);
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest3()
+    {
+        $this->assertTrue(true);
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest4()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest2()
+    {
+        $this->assertTrue(true);
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest3()
+    {
+        $this->assertTrue(true);
+    }
+}
